c3ced50275c4dcd60d578f7cc4f3516fa2579d20
Karpagavadivu <krithiga666@gmail.com>
2024-09-03 16:20:32 +0530

    Add files via upload

---
+++ CustomerIdNotException.java
@@ -0,0 +1,8 @@
+package com.ticket.ticketraise.exception;
+
+public class CustomerIdNotException extends RuntimeException{
+    public CustomerIdNotException(String message){
+        super(message);
+    }
+
+}
